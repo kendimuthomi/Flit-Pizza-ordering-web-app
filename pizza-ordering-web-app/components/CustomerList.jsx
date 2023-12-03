e59b3f6f6b1b9d64f8a58124843418e2307c3818
kendimuthomi <kendijackline42@gmail.com>
2023-12-03 10:38:39 +0300

    Pizza ordering web app UI

--- pizza-ordering-web-app/components/CustomerList.jsx
+++ pizza-ordering-web-app/components/CustomerList.jsx
@@ -2,24 +2,24 @@ import React from 'react'
 
 const CustomerList = [
     {
-        image: "/images/client-1.jpg",
+        image: "/images/client-4.jpg",
         name: "Takar Bowa",
-        review: "The variety of pizzas available is mind-blowing! Whether I'm in the mood for a classic Margherita or feeling adventurous with a loaded BBQ Chicken, this app has it all. Plus, the ability to customize toppings and crust preferences to create my perfect pizza is fantastic.",
+        review: "Lorem ipsum is simply dummy text of the print book. it has survived not only five centuries, but also the leap.",
     },
     {
         image: "/images/client-2.jpg",
         name: "Takar Bowa",
-        review: "The variety of pizzas available is mind-blowing! Whether I'm in the mood for a classic Margherita or feeling adventurous with a loaded BBQ Chicken, this app has it all. Plus, the ability to customize toppings and crust preferences to create my perfect pizza is fantastic.",
+        review: "Lorem ipsum is simply dummy text of the print book. it has survived not only five centuries, but also the leap.",
     },
     {
         image: "/images/client-3.jpg",
         name: "Takar Bowa",
-        review: "The variety of pizzas available is mind-blowing! Whether I'm in the mood for a classic Margherita or feeling adventurous with a loaded BBQ Chicken, this app has it all. Plus, the ability to customize toppings and crust preferences to create my perfect pizza is fantastic.",
+        review: "Lorem ipsum is simply dummy text of the print book. it has survived not only five centuries, but also the leap.",
     },
     {
         image: "/images/client-4.jpg",
         name: "Takar Bowa",
-        review: "The variety of pizzas available is mind-blowing! Whether I'm in the mood for a classic Margherita or feeling adventurous with a loaded BBQ Chicken, this app has it all. Plus, the ability to customize toppings and crust preferences to create my perfect pizza is fantastic.",
+        review: "Lorem ipsum is simply dummy text of the print book. it has survived not only five centuries, but also the leap.",
     },
 ]
 
